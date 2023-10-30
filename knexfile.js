57a8b2a40c14b9d609a760e10631b005c638b9c6
quuamc6n <63206589+quuamc6n@users.noreply.github.com>
2023-10-29 20:34:15 -0600

    env file correction

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: "instockbackend.cdw8he38lb8c.us-east-2.rds.amazonaws.com",
-    user: DB_LOCAL_USER,
-    password: DB_LOCAL_PASSWORD,
-    database: DB_LOCAL_DBNAME,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
+    database: process.env.DB_LOCAL_DBNAME,
     charset: "utf8",
   },
 };
